d456b8611d9e48c07daccaddd83c0d01bf64ab01
o7118 <oduyebojohn123@gmail.com>
2025-01-29 16:19:15 +0100

    commit message

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return response()->json
+        ([
+            'email' => 'oduyebojohn123@gmail.com',
+            'current_datetime' => now(),
+            'github_rl' => 'https://github.com/alpha-king1/hng-stage0.git'
+        ]);
+    }
+}
